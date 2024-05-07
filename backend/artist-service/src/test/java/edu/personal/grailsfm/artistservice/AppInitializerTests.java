c144d3995a69d7f476bce68a2c19a37fa5b66a91
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-07 14:38:24 +0530

    Initialize SpringBoot project for artist microservice

---
+++ backend/artist-service/src/test/java/edu/personal/grailsfm/artistservice/AppInitializerTests.java
@@ -0,0 +1,13 @@
+package edu.personal.grailsfm.artistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInitializerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
