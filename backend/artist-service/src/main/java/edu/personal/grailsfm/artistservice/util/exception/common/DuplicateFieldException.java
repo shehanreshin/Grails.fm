d1fe87790cc8757ebb417ecac85a696c48832bba
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 16:06:22 +0530

    Create an exception to throw when a field value is a duplicate

---
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/util/exception/common/DuplicateFieldException.java
@@ -0,0 +1,7 @@
+package edu.personal.grailsfm.artistservice.util.exception.common;
+
+public class DuplicateFieldException extends RuntimeException {
+    public DuplicateFieldException(String message) {
+        super(message);
+    }
+}
