3e5dc5af09833e625a0819adc01243e3aa75a93b
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 10:16:21 +0530

    Create a validator to validate phone numbers named PhoneNumberValidator

---
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/util/validator/PhoneNumberValidator.java
@@ -0,0 +1,19 @@
+package edu.personal.grailsfm.artistservice.util.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, String> {
+    @Override
+    public void initialize(PhoneNumber constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value == null) {
+            return true;
+        }
+        return value.matches("^[0-9]{10}$");
+    }
+}
