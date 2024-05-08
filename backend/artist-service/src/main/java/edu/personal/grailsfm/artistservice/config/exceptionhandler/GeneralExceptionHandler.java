6c5b030a4c6bd933035d07c1df4c220585261cb5
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 15:59:54 +0530

    Add a method to the general exception handler to handle MethodArgumentNotValidExceptions

--- backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/config/exceptionhandler/GeneralExceptionHandler.java
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/config/exceptionhandler/GeneralExceptionHandler.java
@@ -4,11 +4,15 @@ import edu.personal.grailsfm.artistservice.dto.common.ErrorResponseDto;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 
 @RestControllerAdvice
 public class GeneralExceptionHandler {
@@ -22,5 +26,22 @@ public class GeneralExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponseDto("Operation failed due to invalid or duplicate data", new Date()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        StringBuilder messageBuilder = new StringBuilder();
+        Iterator<ObjectError> errors = ex.getBindingResult().getAllErrors().iterator();
+
+        while (errors.hasNext()) {
+            ObjectError error = errors.next();
+            messageBuilder.append(
+                    String.format("%s: %s", ((FieldError) error).getField(), error.getDefaultMessage())
+            );
+            if (errors.hasNext()) messageBuilder.append(" | ");
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto(messageBuilder.toString(), new Date()));
     }
 }
