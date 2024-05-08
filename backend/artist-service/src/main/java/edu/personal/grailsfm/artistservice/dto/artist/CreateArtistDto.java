6b82690cb6c138108b9eded9519a8b68121d9a0c
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 12:35:45 +0530

    Add examples to CreateArtistDto swagger docs

--- backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/dto/artist/CreateArtistDto.java
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/dto/artist/CreateArtistDto.java
@@ -1,6 +1,7 @@
 package edu.personal.grailsfm.artistservice.dto.artist;
 
 import edu.personal.grailsfm.artistservice.util.validator.annotation.ContactNumber;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
@@ -11,15 +12,19 @@ import lombok.Setter;
 @Setter
 @Builder
 public class CreateArtistDto {
+    @Schema(example = "Michael Scott")
     @NotBlank(message = "Name is mandatory")
     private String name;
 
+    @Schema(example = "msott@dundermufflin.org")
     @Email
     private String email;
 
+    @Schema(example = "scottstots")
     @NotBlank(message = "Password is mandatory")
     private String password;
 
+    @Schema(example = "0763211347")
     @ContactNumber
     private String contactNumber;
 }
