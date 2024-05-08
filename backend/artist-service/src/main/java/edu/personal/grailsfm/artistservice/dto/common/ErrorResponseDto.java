aefc78dab2c9ff3f199aa592dc7a8677692481f3
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 13:12:45 +0530

    Create a dto named ErrorResponseDto to return when an error occurs

---
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/dto/common/ErrorResponseDto.java
@@ -0,0 +1,15 @@
+package edu.personal.grailsfm.artistservice.dto.common;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+
+public record ErrorResponseDto(
+        @NotBlank(message = "Error code is mandatory") String errorCode,
+        @NotBlank(message = "Message is mandatory") String message,
+        @PastOrPresent Date date
+) {
+}
