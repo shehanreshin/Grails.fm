3a6e1eb16ad30f2fab7be06539509d7a4573a85b
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 14:46:37 +0530

    Add a method to the general exception handler to catch exceptions caused by data integrity violations

--- backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/config/exceptionhandler/GeneralExceptionHandler.java
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/config/exceptionhandler/GeneralExceptionHandler.java
@@ -1,6 +1,7 @@
 package edu.personal.grailsfm.artistservice.config.exceptionhandler;
 
 import edu.personal.grailsfm.artistservice.dto.common.ErrorResponseDto;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +15,11 @@ public class GeneralExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleGeneralException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponseDto("An unexpected error has occurred", new Date()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponseDto> handleInvalidDataException(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto("Operation failed due to invalid or duplicate data", new Date()));
     }
 }
