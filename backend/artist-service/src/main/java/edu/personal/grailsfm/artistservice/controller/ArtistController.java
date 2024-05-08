6216003a1468566b68f1546d3961ec999e23da7c
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 22:38:00 +0530

    Apply QOL changes

--- backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/controller/ArtistController.java
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/controller/ArtistController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/artists")
+@RequestMapping("/v1/artists")
 public class ArtistController {
     private final ArtistService artistService;
 
