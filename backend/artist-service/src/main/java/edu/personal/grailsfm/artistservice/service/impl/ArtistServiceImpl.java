5a4c2241f97c8dc3c74cb8e557a5fdbee028792b
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 11:35:57 +0530

    Write basic logic for createArtist method in service layer of artist

--- backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/service/impl/ArtistServiceImpl.java
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/service/impl/ArtistServiceImpl.java
@@ -1,8 +1,11 @@
 package edu.personal.grailsfm.artistservice.service.impl;
 
 import edu.personal.grailsfm.artistservice.dto.artist.CreateArtistDto;
+import edu.personal.grailsfm.artistservice.entity.Artist;
 import edu.personal.grailsfm.artistservice.repository.ArtistRepository;
 import edu.personal.grailsfm.artistservice.service.ArtistService;
+import edu.personal.grailsfm.artistservice.util.enums.ArtistAccountStatus;
+import edu.personal.grailsfm.artistservice.util.mapper.ArtistMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +13,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class ArtistServiceImpl implements ArtistService {
     private final ArtistRepository artistRepository;
+    private final ArtistMapper artistMapper;
 
     @Override
     public Long createArtist(CreateArtistDto artistDto) {
-        return null;
+        Artist artist = artistMapper.map(Artist.class, artistDto);
+        artist.setStatus(ArtistAccountStatus.CONFIRMATION_PENDING);
+
+        Artist savedArtist = artistRepository.save(artist);
+
+        if (savedArtist.getId() == null) {
+            return null;
+        }
+
+        return savedArtist.getId();
     }
 }
