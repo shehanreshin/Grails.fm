c2c2ab7b31ccdb18d7ce8fa49385932d896376f7
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-08 10:47:16 +0530

    Remove constructors and add required args constructor annotation for better readability

--- backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/controller/ArtistController.java
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/controller/ArtistController.java
@@ -2,6 +2,7 @@ package edu.personal.grailsfm.artistservice.controller;
 
 import edu.personal.grailsfm.artistservice.dto.artist.CreateArtistDto;
 import edu.personal.grailsfm.artistservice.service.ArtistService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/artists")
 public class ArtistController {
     private final ArtistService artistService;
-
-    public ArtistController(ArtistService artistService) {
-        this.artistService = artistService;
-    }
 
     @PostMapping
     public ResponseEntity<Long> createArtist(@RequestBody CreateArtistDto artistDto) {
--- backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/service/impl/ArtistServiceImpl.java
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/service/impl/ArtistServiceImpl.java
@@ -3,15 +3,13 @@ package edu.personal.grailsfm.artistservice.service.impl;
 import edu.personal.grailsfm.artistservice.dto.artist.CreateArtistDto;
 import edu.personal.grailsfm.artistservice.repository.ArtistRepository;
 import edu.personal.grailsfm.artistservice.service.ArtistService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class ArtistServiceImpl implements ArtistService {
     private final ArtistRepository artistRepository;
-
-    public ArtistServiceImpl(ArtistRepository artistRepository) {
-        this.artistRepository = artistRepository;
-    }
 
     @Override
     public Long createArtist(CreateArtistDto artistDto) {
