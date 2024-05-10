2063b2517c906b0730b9053c3427016e41a4be90
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-10 14:25:05 +0530

    Initialize SpringBoot project for authentication microservice

---
+++ backend/auth-service/src/test/java/edu/personal/grailsfm/authservice/AppInitializerTests.java
@@ -0,0 +1,13 @@
+package edu.personal.grailsfm.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInitializerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
