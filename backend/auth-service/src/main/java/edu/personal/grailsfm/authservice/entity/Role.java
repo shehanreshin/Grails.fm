766eaf391a74b0a71ce80e3f0884e9caf581bf09
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-10 17:21:13 +0530

    Add equals and hashcode methods to Role entity in auth-service

--- backend/auth-service/src/main/java/edu/personal/grailsfm/authservice/entity/Role.java
+++ backend/auth-service/src/main/java/edu/personal/grailsfm/authservice/entity/Role.java
@@ -1,8 +1,11 @@
 package edu.personal.grailsfm.authservice.entity;
 
 import lombok.*;
+import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -12,7 +15,25 @@ import org.springframework.data.cassandra.core.mapping.Table;
 @Table(value = "role")
 public class Role {
     @PrimaryKey
+    @Column(value = "id")
     private Integer id;
+
+    @Column(value = "name")
     private String name;
+
+    @Column(value = "description")
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
