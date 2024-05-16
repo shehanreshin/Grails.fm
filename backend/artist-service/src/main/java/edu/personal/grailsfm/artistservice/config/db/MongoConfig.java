9f5bda33c38aa2ea2cfac737d30fc38649b3b548
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-16 16:25:35 +0530

    Add configuration to enable mongodb audits in order to make the updatedAt and createdAt fields work

---
+++ backend/artist-service/src/main/java/edu/personal/grailsfm/artistservice/config/db/MongoConfig.java
@@ -0,0 +1,9 @@
+package edu.personal.grailsfm.artistservice.config.db;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
