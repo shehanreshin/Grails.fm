351a2728b4c5916e37f96d7d3fe8db10448cf4e8
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-21 20:57:11 +0530

    Delete support for MP3 for the time being

--- backend/song-service/src/main/java/edu/personal/grailsfm/songservice/service/factory/AudioProcessorServiceFactory.java
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/service/factory/AudioProcessorServiceFactory.java
@@ -1,27 +1,23 @@
 package edu.personal.grailsfm.songservice.service.factory;
 
 import edu.personal.grailsfm.songservice.service.AudioProcessorService;
-import edu.personal.grailsfm.songservice.service.impl.Mp3AudioProcessorServiceImpl;
 import edu.personal.grailsfm.songservice.service.impl.WavAudioProcessorServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 @RequiredArgsConstructor
 public class AudioProcessorServiceFactory {
     private final WavAudioProcessorServiceImpl wavAudioProcessorService;
-    private final Mp3AudioProcessorServiceImpl mp3AudioProcessorService;
 
     public AudioProcessorService create(MultipartFile file) {
         String fileName = file.getOriginalFilename();
-        if (fileName.isBlank() || !fileName.contains(".")) return null;
+        if ((fileName != null && fileName.isEmpty()) || !fileName.contains(".")) return null;
 
         String extension = fileName.strip().substring(fileName.lastIndexOf(".")+1);
         return switch (extension) {
             case "wav" -> wavAudioProcessorService;
-            case "mp3" -> mp3AudioProcessorService;
             default -> null;
         };
     }
