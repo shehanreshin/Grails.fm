e3169ad4a46ff9030dc8707594e763147ebd1e16
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-21 21:22:05 +0530

    Create a global exception handler for track specific exceptions

---
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/config/exceptionhandler/TrackExceptionHandler.java
@@ -0,0 +1,20 @@
+package edu.personal.grailsfm.songservice.config.exceptionhandler;
+
+import edu.personal.grailsfm.songservice.util.exception.TrackCreationException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class TrackExceptionHandler {
+    @ExceptionHandler(TrackCreationException.class)
+    public ResponseEntity<ErrorResponseDto> handleArtistCreationFailedException(TrackCreationException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponseDto(ex.getMessage(), new Date()));
+    }
+}
---
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/dto/common/ErrorResponseDto.java
@@ -0,0 +1,12 @@
+package edu.personal.grailsfm.songservice.dto.common;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.util.Date;
+
+public record ErrorResponseDto(
+        @NotBlank(message = "Message is mandatory") String message,
+        @PastOrPresent Date date
+) {
+}
