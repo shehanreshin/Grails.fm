7ca86967570675646c9d8e18c7a97b6e0e4df96b
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-21 23:53:46 +0530

    Update MongoConfigs to get rid of _class column

--- backend/song-service/src/main/java/edu/personal/grailsfm/songservice/config/MongoConfig.java
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/config/MongoConfig.java
@@ -1,9 +1,21 @@
 package edu.personal.grailsfm.songservice.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableMongoAuditing
-public class MongoConfig {
+public class MongoConfig implements InitializingBean {
+    @Lazy
+    private final MappingMongoConverter mappingMongoConverter;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    }
 }
