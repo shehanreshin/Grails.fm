2d49d639420368c2826e1d6fbde8686f929febc6
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-21 15:33:58 +0530

    Create skeleton for dto used when creating a track

---
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/dto/CreateTrackDto.java
@@ -0,0 +1,16 @@
+package edu.personal.grailsfm.songservice.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public record CreateTrackDto(
+        String title,
+        String artistId,
+        String artistName,
+        String description,
+        MultipartFile file,
+        List<String> genres,
+        String uri
+) {
+}
