788c60403e666e92d6e56961ec5cc11df0d7795e
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-24 13:48:42 +0530

    Create S3 Config bean

---
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/config/S3Config.java
@@ -0,0 +1,36 @@
+package edu.personal.grailsfm.songservice.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("${aws.s3.endpoint}")
+    private String awsEndpoint;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${aws.access-key}")
+    private String awsAccessKey;
+
+    @Value("${aws.secret-key}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsEndpoint, awsRegion))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+                .build();
+    }
+}
