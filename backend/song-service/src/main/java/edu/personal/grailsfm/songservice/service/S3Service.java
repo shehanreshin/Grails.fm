89bfe992961d257760829a63f30467a52b64ee46
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-24 22:41:16 +0530

    Create a method in S3 service layer to download a file from a URI

--- backend/song-service/src/main/java/edu/personal/grailsfm/songservice/service/S3Service.java
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/service/S3Service.java
@@ -1,9 +1,11 @@
 package edu.personal.grailsfm.songservice.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface S3Service {
     String uploadFile(MultipartFile file, String bucketName) throws IOException;
+    Resource downloadFile(String uri) throws IOException;
 }
--- backend/song-service/src/main/java/edu/personal/grailsfm/songservice/service/impl/S3ServiceImpl.java
+++ backend/song-service/src/main/java/edu/personal/grailsfm/songservice/service/impl/S3ServiceImpl.java
@@ -2,11 +2,14 @@ package edu.personal.grailsfm.songservice.service.impl;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.util.StringUtils;
 import edu.personal.grailsfm.songservice.config.S3Config;
 import edu.personal.grailsfm.songservice.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,8 +36,20 @@ public class S3ServiceImpl implements S3Service {
         return String.format("%s/%s/%s", s3Config.getAwsEndpoint(), bucketName, filename);
     }
 
+    @Override
+    public Resource downloadFile(String uri) throws IOException {
+        S3Object s3Object = amazonS3.getObject(getFileNameFromUri(uri), s3Config.getBucketName());
+        return new ByteArrayResource(s3Object.getObjectContent().readAllBytes());
+    }
+
     private String getUniqueFileName(String filename) {
         String newName = UUID.randomUUID().toString();
         return filename == null ? newName : newName + "-" + filename.toLowerCase().replace(" ", "-");
+    }
+
+    private String getFileNameFromUri(String uri) {
+        return uri
+                .replace(String.format("%s/", s3Config.getAwsEndpoint()), "")
+                .replace(String.format("%s/", s3Config.getBucketName()), "");
     }
 }
